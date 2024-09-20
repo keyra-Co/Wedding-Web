94139b14f67d549bfda12f9dfdfcb20fd9a2d853
BagasSaputra666 <bagasbagass891@gmail.com>
2024-09-20 09:27:09 +0700

    fix url base

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,9 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: '/Wedding-Web',
+export default defineConfig(({ mode }) => {
+  return {
+    plugins: [react()],
+    base: mode === 'production' ? '/Wedding-Web/' : '/', // gunakan base path hanya saat production
+  };
 });
